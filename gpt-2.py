82897f17d3edefc0fae36abca86ae04cc06d6149
jositux <jm.guaimas@gmail.com>
2020-10-28 21:30:25 -0300

    Create gpt-2.py
    Comandos 
    https://colab.research.google.com/drive/1Xlkf6WLudhQyH3Y3qj5P_w2eSugo0egC?usp=sharing

---
+++ gpt-2.py
@@ -0,0 +1,224 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
+!pip install transformers==2.9.1
+!pip install pytorch-lightning==0.8.4
+!pip install -q aitextgen
+
+%cd "/content/drive/My Drive/wip"
+from aitextgen import aitextgen
+
+#Para inicialziar el modelo por primera vez se carga así
+ai = aitextgen(tf_gpt2="124M", to_gpu=True)
+
+file_path = "/content/drive/My Drive/wip/ygdrasil.txt" # Ruta del archivo de texto que usare para entrenar al modelo
+ai.train(file_path,
+         line_by_line=False,
+         num_steps=5000,
+         generate_every=500,
+         save_every=100,
+         learning_rate=1e-4,
+         batch_size=1, 
+         )
+         
+ai.generate(n=4, prompt="plata blanda mojada", temperature=0.7, max_length=1024)
+
+
+#Resultados
+
+plata blanda mojada en la eternidad
+del templo.
+
+—No quieres decir que está muerto, pero está de pequeñas cercana.
+Todos callaron como en un estallido que necesitaba.
+—Debes ponerte en esto —amenazó el operador, sin prestar atención
+en el cuchillo.
+El comandante de la ofensiva operativa flotaban era un implicable dentro de
+prueba aénico. Al decir voluntario había abandonado las bases del
+efecto, el sabotaje las cámaras de la sala, una bomba estrella sobre el corazón de
+exorcismo, el pobre tecleado sobre el vértigo, entre sí misma, cada vez más
+y más definido.
+—¡Pinches cabrones sin madre! —recalcó Ramírez a Alvarado, sin mirarlo—. Ahora
+puta las fráficas invengadas de la sala, cada vez más pequeñas rápidamente desde
+la puerta, con sus patrones movimientos dejaban al interior de su ola.
+—Pinches cabrones sin madre —insistió el militar, dejando de tutear
+muerta a alzamiento.
+—No cabes, tú. Cada cabo de años sin madre.
+—Sí, señor —dijo el comandante Meló, y se puso un mal
+probletoro—. ¿Qué pinche cierta?
+Después teando una pequeña escuchada, Mariana nos veía tan rápido.
+La mujer le pregunta entre sus oídos y murió en el aire, evadiendo un colchón de
+exorcismo:
+—Desgraciado…, ¿es un diablo?
+
+—Es que no lo sé. Lo verían que no hubiese de ablar a ninguna mujer adulta. Ese tipo
+podremos estaba colgando de una operación honestad, Mariana.
+La voz sonó dentro de una roca.
+—No comprendo esa del problema, Mariana.
+—Puede saber si una daga como el enemigo, ¿no es verdad? —interrumpió el
+Imbunche, rompiendo al selknam.
+—No quise de pronto el jefe —dijo el Imbunche, que aún sostenía por las
+labullas—. Pero no es humano sé. Lo único que sé es son ni aparatos
+de mí.
+La operación se detuvo y el rostro subalterno por un bolero de Mariana.
+—La demencia está muerta de los acontecimientos —dijo, levantando
+mientras otra vez.
+Ya se había hecho algo en la palabra como un ejemplar de su hermoso
+existencia en un libro de cada palabra que deja parecido a un destrozar.
+—Entonces los construyeron los mismos que vinieron a decenas de voluntarios,
+prefiero en la mente de ese momento adecuado.
+No estés nervioso, ya nada me pude hacer esa mano.
+—Sólo aquellos que no es la mejor elección de la ofensiva, sino que
+te enganchas pueden espantoso como un insecto de seres humanos
+en esta operación. Los vivos entre sus hombros astrales de la Horda Odínica,
+sino que sólo en su interior, un encargo en la operación realizada para sincronizar un
+punto rojo, con una dejada alimentado al acceso: había algo defensivo de la Sección 14.
+La imprimió gritando en el a
+==========
+plata blanda mojada de la actividad kinestésica en el
+suelo de las algas de todo. El día de ida estaba loco su naturaleza,
+engrilladas, con embacabezas de metal al tiempo del software,
+casi hordas de datos en paz y cada columna vertebral recta, se hundían muy
+entre los nervios del Ygdrasil. A la distancia, cada columna vertebral recta, se hundían
+entremamente en las baldosas aislantes de la Sección 14, a la vez que matar por
+donde se hundían después de conseguir la conciencia. El columna
+de regreso a la inactividad kinestésica puede producía conmigo, y la
+ingresó corte religiosa por mantener aparatos que en la intranet de la Chrysler
+de desplegar, buscando la política internacional, a la estructura estatal con
+el Mesías en persona.
+Después del patio de la, el Imbunche formó su propio bísono y sució
+pulpo. Mariana se situó dejen las temas y se pegó la espalda en su rostro.
+—El muchacho nos van a informar que me lo aseguró todo el itinerario.
+—Por supuesto —murmuró.
+—¿Por qué vas a darle la das manos cuando me fijó en ti?
+—No tengo idea. Anún muy bien —dijo el Imbunche, y luego me
+ofrecido por su rostro hacia él.
+Mariana hizo un gesto de su placer, pero la insevedó aprovencia.
+—Quédate ahí, déjame solo.
+—Esto te interesa estaremos a través de la ofensiva debe retraúcar la lada del
+Directorio. Te interesa lo que querías hacer aquí, pero no te preocupes.
+—Esto no tiene nada que debe retir al comandante de la lengua del operario.
+No está físicamente en estado sino. El resto de los operarios te va a atacarnos.
+Cinco según lado.
+Mariana estiró los jefes y se encontró con un pigmento que el selknam
+colgó lentamente de su oído y se hizo espeso, para arrastrándole al cielo.
+—¿Cómo te llamas, pendejo?
+—Sí. Cuando sólo sé a tu puto complicado, déjame solo.
+Mariana percibió las pequeñas y se recoró de pronto.
+—¡Tú, maldito hijo de toda desesperanza, pendejo! Aquí en Düren
+hecho ya!
+Sonrió apresuradamente.
+—No sé cómo eres tú, Mariana. Lo único que parece coche —balbó secamente el
+último—. Aquí en la sala del túnel, el Graal.
+—¡No través de gracia, Rodrigo! —exclamó el Reche, con inusual excitación—. Sólo
+me detengo.
+El selknam la siguiente comenzó a volar.
+—¿No quieres decir que éste pareció una guerra? —gritó la mujer,
+caminando una curiosidad adosada.
+—No te preocupes, nadie va a hacer Ramírez.
+—No estés a salvo, Ramírez. Te lo preocupes.
+—¿Prepararme para explicarle a este animal?
+—Un ser cruel y un grupo para impuro, con la preocupación y además.
+Algo está ocurriendo.
+
+plata blanda mojada por la actividad de media tarde,
+ritualmente el sistema de seguridad y sin concierto.
+Había intentado enterrarle al basurero de la carrera, abrazada con su
+médula espinal que no subía. Estaba de invisibilidad, en posición fetal, de
+ASIL con que en cama apenas o no sólo podía entrar en una matriz de cien metría.
+—Perfecto —murmuró el Imbunche, muy concentrado—. Pero siempre había teclear o
+tejido el rostro negro de una red innace y furia.
+—Extraña conducta —dijo el profeta, y el miedo desgraciado—. Estamos ansos
+tan tiempo que comenzarán a dar a luz.
+—El lugar donde nadie salía tan raro en una habitación terrible para nosotros,
+comandante.
+—No te preocupes, nadie va a aliarse por los enemigos de la fe, y odesa vez lo que
+descubría una perra. El resultado final teógago entonces porque no hay
+permanecer ocurrido. Ese tipo de manipulación sicológica por los nervios
+admitemos.
+El Imbunche estaba placado, salió una roca espinal de la roca con la mano
+dolorososa. Luego, con los brazos de placenta asuncia, salió una
+obra de la boca con una mano. Lentamente, le extrañaba el aire y le
+habría que extrañaba su rostro se acercaba.
+—Espero que todo esto parecido a la mujer —dijo, levantando la voz.
+—No debes temer —insistió el profeta con voz extraña—. Eres demasiado importante
+hacia el solo labor del mundo, y mariana pudo ver en este preciso más de una
+compasión.
+—¿Y qué vamos a hacer con él? —preguntaó la mujer.
+—Ya esté en los precisos —dijo el Imbunche.
+—Por supuesto —agregó.
+—Por los antecedentes adjuntos —agregó, pero el muchacho—.
+Nuestras conversaciones se han envolvivir al gatito humano.
+—¿Ya sabes cuánto bien? —preguntó el profeta, asomándose una mujer.
+- Página—. ¿Nuestras conversaciones te aclor? Antes dime en México.
+¿Y qué contenía eso?
+—En tanto la fe están los que nos podremos hacerlo.
+—Por los antecedentes adjuntos —murmuró ella, sonriente.
+
+El Imbunche la miró casi parecir con su lengua bífida, desvanecida.
+—Entonces dímela, señor.
+—¿No te parecí una ducha? —preguntó el profeta, entre sollozos—. Y
+No es adecuado contestar esa ducha.
+Mariana se detuvo con su mano. Se preguntaba si realmente se
+procedió, ¿qué si realmente se solicitaba tus genteres? ¿Qué puedes hacerlo por
+qué no lo haces de inmediato?
+—Alcor cruz nuestros personal consistían nuestras hijos para el Directorio. Tienes
+deseasible confiar totalmente; no su personal delirio pero en nuestros años.
+Mariana no perdería que sonreír en su disolución.
+—¿Por qué se alimentar a tus genitales?
+—Que así se refera los ojos y eso o no
+==========
+plata blanda mojada de su
+marina escarga. Pero los monitores están preguntados, y una cuestión en la espalda,
+- Página 170
+
+encontrario por las espaldas a la columna del recipiente, en perfecto que
+parecía hacerlo.
+—Perfecto —murmuró el Imbunche, e hizo un par de minutos.
+—¿Y qué ocurre? —preguntó el profeta, como si fuera una forma
+del sabor acre en el carpeta de acceso.
+—Poco puedo ver…
+—Porque sin él jamás. Puedes toma la forma de un objeto —dijo Imbunche,
+preocupado de otras y aficionado a la pared de la columna—. No está cremas
+desapareciendo un espejismo. Creo que es un objeto —se burló la cabeza—. Y
+desde peces ivello, creo que sólo después de que se había hecho
+estoy en mil pedazones de poder.
+—Creo que es un pequeño agujero —repuso el Imbunche, y luego hizo
+protegido por una por las uñas de combate.
+El profeta se acercó a su asistente mismo y el Imbunche percibió ser una
+carnada colgando de órdenes a la pared.
+—Hijo, ve a tu casa.
+—No estés nerviosa, nada malo va a ocurrir —le excedó otro.
+Cumplía su cuarta hora antiguo. Correr por la puerta está dentro de
+su cuartilio.
+—¿Qué dijiste?
+—Mientras siete las oleadas como vacas.
+—Nada —dijo el Imbunche, y sólo se habían cerrado al púbancho.
+—¿Por qué lo es, hizo por tus equipos de encontrar la pared?
+—No me asustes, Rodrigo. Estamos alegrado un desastre.
+Templo Mayor de los Cuernos de Hattin.
+—No es tan terrible.
+—¿Cómo puedes llegar al Graal, Rodrigo?
+—Tú también sabes cómo salir de la conciencia de los mexicanos.
+
+Todos se veían muy desvanecidados.
+—No es tan terrible.
+—Tú no vas a hacer cosas —dijo, fijamente la roca negra.
+El Imbunche miró al agua fría en su rostro.
+—Dijo, oír, creo que sólo aquellos de santa…
+—¡El Graal!
+—¡Entonces los transbordadores con los tontos!
+El Tangata Manu se acercó a su asistente durante.
+—¡No! Cuando el paisaje se contrajo para el enemigo, Tangata Manu
+. ¿Me escuchaste, Ramales?
+—No tienes que escuchaste, Mariana. Te entiemos cómo salir de la conciencia y
+te entiemos terrible.
+Mariana apretó las mandíbulas. «No hay medida de la respuesta», pensó el
+Imbunche.
+—¿Medio México de la especie de trabajar para el éxito?
+—No es adecuado contestar todo esto. Debemos Debemos
+soportar toda la respuesta el Graal. A los minutos, a los mexicanos, a la
+permosa…
+—¿Y qué sabes tú? —preguntó el aplomo, mareada por la espalda y salió de la columna
+—. ¡Entonces está todo!
+El Tangata Manu giró la cabeza y se quedó mirando
